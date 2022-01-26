52d63ace27740e0e10c6005195368e4f511c70bb
Pieter Zitman <pieterzitman@hotmail.com>
2022-01-26 22:31:29 +0100

    Outline board

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <template>
-  <h1>Hello World!</h1>
+  <h1>GIS Memory Game!</h1>
+  <section class="game-board">
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+  </section>
 </template>
 
 <script>
@@ -18,5 +36,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.card {
+  border: 5px solid #ccc;
+}
+
+.game-board {
+  display: grid;
+  grid-template-columns: 15vw 15vw 15vw 15vw;
+  grid-template-rows: 10vh 10vh 10vh 10vh;
+  grid-column-gap: 30px;
+  grid-row-gap: 30px;
+  justify-content: center;
 }
 </style>
